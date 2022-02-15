ec48225ad03e5065bf35e0f4d5678c96604d0eab
seohee0112 <ghdtj7@naver.com>
2022-02-15 14:21:03 +0900

    멀쩡한 사각형

---
+++ Seohee/Lv2/멀쩡한 사각형.js
@@ -0,0 +1,17 @@
+function solution(w, h) {
+    var answer = 0;
+    let dots = [];
+
+    let front_y, end_y;
+
+
+    function solution(w, h) {
+        let answer = 0;
+
+        for (let x = 1; x <= w; x++) {
+            const front_y = (-1) * (h) * (x - 1) / w + h;
+            const end_y = (-1) * (h) * x / w + h;
+            answer += (Math.ceil(front_y) - Math.floor(end_y));
+        }
+        return w * h - answer;
+    }
