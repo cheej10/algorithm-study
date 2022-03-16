f3004504855bef73b671958265a27cbc25763def
seohee0112 <ghdtj7@naver.com>
2022-03-16 12:31:31 +0900

    삼각 달팽이

---
+++ Seohee/Lv2/삼각 달팽이.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  const arrays = new Array(n).fill().map((_, index) => new Array(index + 1));
+
+  let row = 0;
+  let col = -1;
+  let count = 1;
+
+  //삼각형은 3변이므로!
+  for (let i = n; i > 0; i -= 3) {
+    // 왼쪽 -> 아랫 -> 오른쪽 탐색
+    for (let j = 0; j < i; j += 1) arrays[++col][row] = count++;
+
+    for (let j = 0; j < i - 1; j += 1) arrays[col][++row] = count++;
+
+    for (let j = 0; j < i - 2; j += 1) arrays[--col][--row] = count++;
+  }
+
+  return arrays.flat();
+}
